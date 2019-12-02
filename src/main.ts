a02b1654a11a008eee49b5992506fd98786357e5
Martin Flodin <martin.flodin@svt.se>
2019-12-02 14:17:05 +0100

    Day 2, part 2

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,31 @@
 import { readFileSync } from "fs";
 import { intcode } from "./2";
 
-const code = String(readFileSync("./input/2.txt"))
-  .split(",")
-  .filter((x) => x)
-  .map(Number);
+function runner() {
+  const code = String(readFileSync("./input/2.txt"))
+    .split(",")
+    .filter((x) => x)
+    .map(Number);
 
-code[1] = 12;
-code[2] = 2;
+  const GOAL = 19690720;
 
-const res = intcode(code);
-console.log(res[0]);
+  for (let noun = 0; noun <= 99; noun++) {
+    for (let verb = 0; verb <= 99; verb++) {
+      // console.log({ noun, verb });
+      let tempCode = [...code];
+      tempCode[1] = noun;
+      tempCode[2] = verb;
+      const res = intcode(tempCode);
+      if (res[0] === GOAL) {
+        return { noun, verb };
+      }
+    }
+  }
+
+  throw new Error("Goal not found");
+}
+const { noun, verb } = runner();
+
+// console.log({ noun, verb });
+
+console.log(noun * 100 + verb);
